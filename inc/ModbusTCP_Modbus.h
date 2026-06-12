746e1e90786fee57d765229e79ca20a023ef8e51
EMIC Compiler <emic-compiler@emic.io>
2026-06-12 19:36:59 -0300

    Code generation: 2026-06-12 22:36:59 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: eth1
    Project: testethernet
    User: mariano-hunkeler-at-rfindustrial-com

--- inc/ModbusTCP_Modbus.h
+++ inc/ModbusTCP_Modbus.h
@@ -4,11 +4,13 @@
 #include <stdint.h>
 
 #ifndef persist_
-#define persist_ __attribute__((section(".persist")))
+#define _PERSIST_STR2(x) #x
+#define _PERSIST_STR(x) _PERSIST_STR2(x)
+#define persist_ __attribute__((section(".persist." _PERSIST_STR(__COUNTER__))))
 #endif
-extern char persist_ ModbusTCP_Modbus_ip[16];
-extern char persist_ ModbusTCP_Modbus_mac[18];
-extern uint16_t persist_ ModbusTCP_Modbus_port;
+extern char ModbusTCP_Modbus_ip[16];
+extern char ModbusTCP_Modbus_mac[18];
+extern uint16_t ModbusTCP_Modbus_port;
 
 void ModbusTCP_Modbus_init(void);
 
